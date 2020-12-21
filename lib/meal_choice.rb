d3bfe30f3a055497f11c098270c7c693ff4c8cf8
Jade McDaniels <jademcdaniels81@gmail.com>
2020-12-21 01:51:14 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,7 +2,7 @@
 
 def meal_choice (veg1, veg2, protein = "tofu")
    "What a nutritious meal!"
-  p "A plate of #{protein} with #{veg1} and #{veg2}."
+  print "A plate of #{protein} with #{veg1} and #{veg2}."
 end 
 
 
